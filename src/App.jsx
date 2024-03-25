21e5b39a1ae275c17be4ce351ee8ddf822af5028
Desmond <daworam@gmail.com>
2024-03-25 16:58:33 -0500

    mapped throught puppies, section 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
-
+import {puppyList} from './data.js'
+import { useState } from 'react'
 
 function App() {
-
-
+  console.log(puppyList)
+  const [puppies, setPuppies] = useState(puppyList)
   return (
     <>
-      
+    {
+      puppies.map((puppy, index) => {
+        return <p key={index}>{puppy.name}</p>
+      })
+    }
     </>
   )
 }
